d913f28147e21493e976f4a9f6b0588e182a6556
Seunggeun Yoon <tmdmrs522@gmail.com>
2024-07-10 00:06:35 +0900

    second commit

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const API_KEY = `203207a5e7ec4700b1717e879ed1396a`;
 let news = [];
 const getLatestNews = async () => {
   const url = new URL(
-    `https://yoon-newsapi.netlify.app//top-headlines?country=kr&apikey=${API_KEY}`
+    `https://yoon-newsapi.netlify.app//top-headlines?country=kr`
   );
+  //   &apikey=${API_KEY}
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
